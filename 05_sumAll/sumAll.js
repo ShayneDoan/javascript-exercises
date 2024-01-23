15d4e224eb7793219c168c352b38383bc4d7f8bf
shayaansufi <shayaansufi@gmail.com>
2024-01-23 17:29:11 -0500

    Add function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    if (firstNumber < 0 || secondNumber < 0) {
+        return "ERROR";
+    }
+    if (typeof firstNumber != "number" || typeof secondNumber != "number") {
+        return "ERROR";
+    }
+    let sum = 0;
+    if (firstNumber > secondNumber) {
+        let temp = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = temp;
+    }
+    for (let i = firstNumber; i <= secondNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
